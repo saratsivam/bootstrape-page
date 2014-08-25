5da368044088e3f2651e16be6b1dcf1ff9c54f54
sarat <saratxtech@gmail.com>
2014-08-26 01:25:43 +0530

    animation code added

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,39 @@
 $(document).ready(function(){
+	
+	
 	prettyPrint();
-})
+	
+	
+	canvas = $('#canvas');
+	  g = GUtil.createGraphis({
+    		target: '#canvas'	
+  	         });
+	
+	
+	
+	
+	
+});
+
+
 $('.custom-nav-tabs a').click(function (e) {
 	e.preventDefault();
 	$(this).tab('show');
-})
 	
+	if($(this).text()=='Result'){
+		showResult($(this));
+	}	
+});
+
+
+function showResult(x){
+  codeDiv = $(x.parent().prev().children().attr('href'));
+  resultDiv = $(x.attr('href'));	
+	resultDiv.html(canvas);
+	g.clearLoops();g.clear();
+	eval(codeDiv.text().replace(/\/\*.+?\*\/|\/\/.*(?=[\n\r])/g, ''));
+
+}
+
+
+

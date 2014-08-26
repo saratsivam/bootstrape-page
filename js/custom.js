e29c5bd601cb2e41aefc10c2056927de1f5a0485
saratsivam <saratxtech@gmail.com>
2014-08-26 19:09:15 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -18,11 +18,16 @@ $(document).ready(function(){
 
 $('.custom-nav-tabs a').click(function (e) {
 	e.preventDefault();
+	$('.custom-nav-tabs li:first-child a').tab('show');	
 	$(this).tab('show');
 	
 	if($(this).text()=='Result'){
 		showResult($(this));
-	}	
+	}else{
+		g.clearLoops();
+		console.log('stopped.....')
+	}
+	
 });
 
 
@@ -30,7 +35,7 @@ function showResult(x){
   codeDiv = $(x.parent().prev().children().attr('href'));
   resultDiv = $(x.attr('href'));	
 	resultDiv.html(canvas);
-	g.clearLoops();g.clear();
+	g.clearLoops();g.clear();g.x=0;g.y=0;g.color='white';
 	eval(codeDiv.text().replace(/\/\*.+?\*\/|\/\/.*(?=[\n\r])/g, ''));
 
 }
